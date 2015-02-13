dadceb4c7d92e5c05ec90e5988ef4971259ce2ef
Ruben Barilani <ruben.barilani.dev@gmail.com>
2015-02-13 13:45:50 +0100

    simplifies configuration of dependent symfony cmf routing bundle

--- DependencyInjection/TruelabKottiFrontendExtension.php
+++ DependencyInjection/TruelabKottiFrontendExtension.php
@@ -4,6 +4,7 @@ namespace Truelab\KottiFrontendBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class TruelabKottiFrontendExtension extends Extension
+class TruelabKottiFrontendExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -32,5 +33,40 @@ class TruelabKottiFrontendExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $config = array(
+            'chain' => array(
+                'routers_by_id' => array(
+                    'cmf_routing.dynamic_router' => 100,
+                    'router.default' => 200,
+                )
+            ),
+            'dynamic' => array(
+                'enabled' => true,
+                'default_controller' => 'TruelabKottiFrontendBundle:Context:view',
+                'route_provider_service_id' => 'truelab_kotti_frontend.route_provider'
+            )
+        );
+
+        foreach ($container->getExtensions() as $name => $extension) {
+
+            switch ($name) {
+                case 'cmf_routing':
+                    // set use_acme_goodbye to false in the config of
+                    // acme_something and acme_other note that if the user manually
+                    // configured use_acme_goodbye to true in the app/config/config.yml
+                    // then the setting would in the end be true and not false
+                    $container->prependExtensionConfig($name, $config);
+                    break;
+            }
+        }
     }
 }
